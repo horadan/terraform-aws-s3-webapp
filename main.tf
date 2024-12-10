4218fcc849d25674453a2eb17103e33eee5afd7c
Danny Hora <98090741+horadan@users.noreply.github.com>
2024-12-10 18:49:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "aws_s3_bucket_acl" "bucket" {
 
 resource "aws_s3_bucket_policy" "policy" {
   depends_on = [
-    aws_s3_bucket_acl.bucket
+    aws_s3_bucket_acl.bucket,
   ]
   bucket = aws_s3_bucket.bucket.id
   policy = <<EOF
